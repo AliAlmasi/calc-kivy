7b7686c192ec8a48d878003f9b014c7e5fe2d60b
Ali Almasi <a710almasi@gmail.com>
2025-10-28 02:27:24 +0330

    feat: added fonts fix: removed kivy configs and added windows ctypes

--- calc.py
+++ calc.py
@@ -1,12 +1,50 @@
 import ast
 import operator as op
+from kivy import require
 from kivy.app import App
-from kivy.config import Config
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.metrics import dp
+import ctypes
+
+require('2.0.0')
+_kivy_file = "./calc.kv"
+
+# get the native window handle
+hwnd = ctypes.windll.user32.GetActiveWindow()
+
+# window styles
+GWL_STYLE = -16
+WS_MAXIMIZEBOX = 0x00010000  # maximize button
+WS_THICKFRAME   = 0x00040000  # resizable border
+
+# get current style
+style = ctypes.windll.user32.GetWindowLongW(hwnd, GWL_STYLE)
+
+# remove maximize button and resize border
+style &= ~WS_MAXIMIZEBOX
+style &= ~WS_THICKFRAME
+
+# apply new style
+ctypes.windll.user32.SetWindowLongW(hwnd, GWL_STYLE, style)
+
+FONTS = {
+    "regular": 'data/fonts/JetBrainsMono-Regular.ttf',
+    "italic": 'data/fonts/JetBrainsMono-Italic.ttf',
+    "bold": 'data/fonts/JetBrainsMono-Bold.ttf',
+    "bold_italic": 'data/fonts/JetBrainsMono-BoldItalic.ttf',
+}
+
+def get_font(bold=False, italic=False):
+    if bold and italic:
+        return FONTS["bold_italic"]
+    elif bold:
+        return FONTS["bold"]
+    elif italic:
+        return FONTS["italic"]
+    return FONTS["regular"]
 
 
 ALLOWED_OPERATORS = {
@@ -94,18 +132,12 @@ class CalcLayout(BoxLayout):
             self.display = "Error"
 
 class CalculatorApp(App):
-    def __init__(self):
-        super().__init__()
-        self.configure()
-
-    def configure(self):
-        Config.set("graphics", "resizable", 0)
-        Config.set("graphics", "fullscreen", 0)
-        Config.write()
+    def get_font(self, **kwargs):
+        return get_font(**kwargs)
     
     def build(self):
         self.title = "Ali Almasi - Calculator"
-        Builder.load_file("./calc.kv")
+        Builder.load_file(_kivy_file)
         Window.size = (dp(400), dp(710))
         Window.bind(on_key_down=self._on_keyboard_down)
         return CalcLayout()
